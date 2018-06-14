c4560aeaffa1d1ad3ed1774eba0e3713be6c8a47
WilsonLTL <wilsonlo1997@gmail.com>
2018-06-14 17:06:28 +0800

    update

--- application.py
+++ application.py
@@ -177,10 +177,7 @@ def api_article3():
         result = get_user_db()
         if result is not None:
             for items in result:
-                try:
-                    item = result[items]
-                except Exception as ex:
-                    print("EX:",ex)
+                item = result[items]
                 if item['user_id'] == user_id:
                     item['user_image'].append(image_str)
                     result = insert_exist_user(item,items)
